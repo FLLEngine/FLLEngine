58487791e3e2998baa72158747d192e6b4553186
imposiblaa <imposiblaa@gmail.com>
2021-03-29 17:12:39 -0700

    added support for colors indicating different grid weights

--- pathfind.py
+++ pathfind.py
@@ -7,19 +7,25 @@ from pathfinding.finder.a_star import AStarFinder
 
 def doPathFind(pathToPng,startX, startY, endX, endY, createImg, pathToBackground):
     numpy.set_printoptions(threshold=sys.maxsize)
-    decoded = cv2.imread(pathToPng, 0)
+    image = cv2.imread(pathToPng, 1)
     tempList = []
-    xi, yi = 0,0
-
-    for x in decoded:
+    imageShape = image.shape
+    decoded = numpy.empty((imageShape[0],imageShape[1]))
+    xi = 0
+    for x in image:
+        yi = 0
         for y in x:
-            if y>0:
+            if y.tolist()==[0,0,0]:
+                decoded[int(xi)][int(yi)] = 0
+            elif y.tolist()==[255,255,255]:
+                decoded[int(xi)][int(yi)] = 2
+            elif y.tolist()==[0,255,0]:
                 decoded[int(xi)][int(yi)] = 1
+            elif y.tolist()==[255,0,0]:
+                decoded[int(xi)][int(yi)] = 3
 
-            
             yi += 1
         
-        yi = 0
         xi += 1
 
     matrix = decoded    
@@ -34,7 +40,6 @@ def doPathFind(pathToPng,startX, startY, endX, endY, createImg, pathToBackground
     if createImg:
         toEncode = cv2.imread(pathToBackground, 1)
         for coords in path:
-            print(coords)
             toEncode[coords[1]][coords[0]] = (0, 255, 0)
 
         cv2.imshow('image', toEncode)
