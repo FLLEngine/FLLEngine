c7f6b1c92a665bd58848ce33a6e148a600a0f0d9
imposiblaa <imposiblaa@gmail.com>
2021-09-30 22:20:56 -0700

    working C extension (dev starts tomorrow)

--- fllengine.c
+++ fllengine.c
@@ -1,25 +1,26 @@
 #include "py/runtime.h"
 
 #include <stdio.h>
+#include <string.h>
 
-STATIC mp_obj_t fll_test(mp_obj_t text_obj){
+const char init_line[] = "Go Mindbenders!";
 
-    char txt[] = mp_obj_get_str(text_obj);
+STATIC mp_obj_t fll_test() {
+    printf("%s\n", init_line);
+    return mp_const_true;
+}
 
-    printf("%s\n", txt);
-};
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(fll_test_obj, fll_test);
 
-STATIC MP_DEFINE_CONST_FUN_OBJ_2(fll_test_obj, fll_test);
-
-STATIC const mp_rom_mapp_elem_t fll_module_globals_table[] = {
+STATIC const mp_rom_map_elem_t fll_module_globals_table[] = {
 { MP_ROM_QSTR(MP_QSTR__name__), MP_ROM_QSTR(MP_QSTR_fllengine) },
 { MP_ROM_QSTR(MP_QSTR_fll_test), MP_ROM_PTR(&fll_test_obj) },
 };
 
-STATIC MP_DEFINE_CONST_DICT(fll_module_globals, example_module_globals_table);
+STATIC MP_DEFINE_CONST_DICT(fll_module_globals, fll_module_globals_table);
 
 const mp_obj_module_t fll_engine_cmodule = {
-.base = { $mp_type_module },
+.base = { &mp_type_module },
 .globals = (mp_obj_dict_t *)&fll_module_globals,
 };
 
