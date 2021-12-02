07752f72979cd01ad84e676328c3295acbcb682a
imposiblaa <imposiblaa@gmail.com>
2021-12-01 19:43:09 -0800

    finished arg parsing and math for toLoc

--- src/fllengine.c
+++ src/fllengine.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <math.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <pthread.h>
@@ -16,10 +17,13 @@
 const char init_line[] = "Go Mindbenders!";
 pthread_t gyroThread;
 double gyroAngle;
-short gyroValue;
+short gyroValue = 0;
 char location[35];
 
+
+// returns the sensor directory with the gyro most recently connected to the robot
 char *findGyro() {
+    printf("finding gyro\n");
     struct dirent *dir;
     DIR *rootdir = opendir("/sys/class/lego-sensor/");
     if (rootdir == NULL) {
@@ -48,7 +52,10 @@ char *findGyro() {
     return false;
 }
 
+
+// calls findGyro(), calibrates using data collected over a loop whose length is controlled with argument 0, starts and infinite loop calculating the current angle of the gyro and writing it to gyroAngle
 void *GyroAng(void *calibration_number_void) {
+    printf("starting gyro\n");
     int calibration_number;
     calibration_number = (int)calibration_number_void;
     printf("starting gyro\n");
@@ -96,10 +103,39 @@ void *GyroAng(void *calibration_number_void) {
     return NULL;
 }
 
-//STATIC mp_obj_t fll_init() {
-//    pthread_t gyroThread;
-//}
+void mp_array_to_array(mp_obj_tuple_t **mp_array, int ** arr) {
+    * arr = malloc(2 * sizeof(int));
+    for(int i=0; i<2;i=i+1) {
+        (*arr)[i] = mp_obj_get_int((*mp_array)->items[i]);
+    }
+}
 
+
+
+//
+STATIC mp_obj_t fll_to_loc(mp_obj_t currentLocObj, mp_obj_t targetLocObj, mp_obj_t speedObj) {
+    int speed = mp_obj_get_int(speedObj);
+
+    mp_obj_tuple_t *currentLocTObj = MP_OBJ_TO_PTR(currentLocObj);
+    mp_obj_tuple_t *targetLocTObj = MP_OBJ_TO_PTR(targetLocObj);
+    int *currentLoc;
+    int *targetLoc;
+    mp_array_to_array(&currentLocTObj, &currentLoc);
+    mp_array_to_array(&targetLocTObj, &targetLoc);
+
+    float targetAngle = (atan2(targetLoc[0]-currentLoc[0], targetLoc[1]-currentLoc[1]))*57.29578;  //strange number is radians to degrees conversion
+    if(targetAngle<0){
+        targetAngle = 360+targetAngle;
+    }
+
+    printf("from: (%d, %d) | To: (%d, %d) | Speed: %d | Reletive Angle: %f\n", currentLoc[0], currentLoc[1], targetLoc[0], targetLoc[1], speed, targetAngle);
+    return mp_const_true;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(fll_to_loc_obj, fll_to_loc);
+
+
+
+// creates a thread that runs the GyroAng() function, argument 0 is passed through to argument 0 of GyroAng()
 STATIC mp_obj_t fll_start_gyro(mp_obj_t calibration_num_obj) {
     int calibration_num = mp_obj_get_int(calibration_num_obj);
     pthread_t gyroThread;
@@ -108,6 +144,9 @@ STATIC mp_obj_t fll_start_gyro(mp_obj_t calibration_num_obj) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(fll_start_gyro_obj, fll_start_gyro);
 
+
+
+// terminates the thread running GyroAng()
 STATIC mp_obj_t fll_stop_gyro() {
     pthread_cancel(gyroThread);
     return mp_const_true;
@@ -115,41 +154,44 @@ STATIC mp_obj_t fll_stop_gyro() {
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(fll_stop_gyro_obj, fll_stop_gyro);
 
 
+
+// prints argument 0 and its length
 STATIC mp_obj_t fll_test(mp_obj_t text_mp_obj) {
     mp_check_self(mp_obj_is_str_or_bytes(text_mp_obj));
     GET_STR_DATA_LEN(text_mp_obj, pre_text, text_len);
     char text[text_len];
     strcpy(text, (char *)pre_text);
     printf("%s: %d\n", text, text_len);
-    /* struct timeval before, after; */
-    /* gettimeofday(&before,NULL); */
-    /* sleep(2); */
-    /* gettimeofday(&after,NULL); */
-    /* double time_clocks = ((after.tv_usec - before.tv_usec) + ((after.tv_sec-before.tv_sec)*1000000))/1000000; */
-    /* printf("%f",time_clocks); */
     return mp_const_true;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(fll_test_obj, fll_test);
 
+
+
+// enters an infinite loop that prints the current angle of the gyro
 STATIC mp_obj_t fll_watch_gyro(mp_obj_t sample_rate_obj) {
     //int sample_rate = mp_obj_get_int(sample_rate_obj);
     //int delay = 1000/sample_rate;
     while(true){
         sleep(1);
-        printf("%f | %d\n", gyroAngle, gyroValue);
+        printf("%f\n", gyroAngle);
     }
     printf("returning\n");
     return mp_const_true;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(fll_watch_gyro_obj, fll_watch_gyro);
 
+// micropython stuff ===================================================================================================================
+// =====================================================================================================================================
 
+// all functions that can be called from the mp library go here
 STATIC const mp_rom_map_elem_t fll_module_globals_table[] = {
 { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_fllengine) },
 { MP_ROM_QSTR(MP_QSTR_test), MP_ROM_PTR(&fll_test_obj) },
 { MP_ROM_QSTR(MP_QSTR_watchGyro), MP_ROM_PTR(&fll_watch_gyro_obj) },
 { MP_ROM_QSTR(MP_QSTR_startGyro), MP_ROM_PTR(&fll_start_gyro_obj) },
 { MP_ROM_QSTR(MP_QSTR_stopGyro), MP_ROM_PTR(&fll_stop_gyro_obj) },
+{ MP_ROM_QSTR(MP_QSTR_toLoc), MP_ROM_PTR(&fll_to_loc_obj) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(fll_module_globals, fll_module_globals_table);
